61a138fd54102e6337a0236819db27893c1b5315
AnaMolly <analia.mohamdally@gmail.com>
2021-05-20 18:32:49 -0400

    added if and for statements for the password criteria

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var lcArray= [["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]];
+var lcArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var ucArray=["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numArray=["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 var scArray=["+", "-", "#", "%", "!", "(", ")", "{", "}", "/", "]", "@", "~", "*", "?", ":","$", "&","'", "[", ",",";", "=", ">"];
 var passwordArray=[];
+var finalPasswordArray=[];
 
 
 
@@ -32,11 +33,39 @@ function writePassword() {
 
   if (lcInquiry != true && ucInquiry != true && numInquiry != true && scInquiry != true){
     window.alert ("You must select one character type.")
+    lcInquiry = window.confirm("Do you want lowercase letters in your password?")
+    ucInquiry = window.confirm("Do you want uppercase letters in your password?")
+    numInquiry = window.confirm("Do you want numbers in your password?")
+    vscInquiry = window.confirm("Do you want special characters in your password?")
   }
 
+  if (lcInquiry==true){
+    for(i=0;i<=letterAmoutInquiry-1;i++){
+      passwordArray.push(lcArray[Math.floor(Math.random()*lcArray.length)])
+    }
+  }
+  if (ucInquiry==true){
+    for(i=0;i<=letterAmoutInquiry-1;i++){
+      passwordArray.push(ucArray[Math.floor(Math.random()*ucArray.length)])
+    }
+  }
+  if (numInquiry==true){
+    for(i=0;i<=letterAmoutInquiry-1;i++){
+      passwordArray.push(numArray[Math.floor(Math.random()*numArray.length)])
+    }
+  }
+  if (scInquiry==true){
+    for(i=0;i<=letterAmoutInquiry-1;i++){
+      passwordArray.push(scArray[Math.floor(Math.random()*scArray.length)])
+    }
+  }
+  console.log(passwordArray);
+    
+     
   
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+
+  //var password = generatePassword();
+  //var passwordText = document.querySelector("#password");
 
   
 
@@ -44,7 +73,7 @@ function writePassword() {
 
 
 
-  passwordText.value = password;
+  //passwordText.value = password;
 
 }
 
