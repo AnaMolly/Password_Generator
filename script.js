3f085b588393cff60f2484496137dff046dd8615
AnaMolly <analia.mohamdally@gmail.com>
2021-05-20 16:59:09 -0400

    added variables to js file

--- script.js
+++ script.js
@@ -5,10 +5,24 @@ var ucArray=["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N
 var numArray=["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 var scArray=["+", "-", "#", "%", "!", "(", ")", "{", "}", "/", "]", "@", "~", "*", "?", ":","$", "&","'", "[", ",",";", "=", ">"];
 
+
+
+
 // Write password to the #password input
 function writePassword() {
+  var initialAlert = window.alert("Let's generate a password! Please answer the following criteria questions.")
+  var letterAmoutInquiry = window.prompt ("How long do you want your password to be? You can choose bewtween 8-128 characters.")
+  var lcInquiry = window.confirm("Do you want lowercase letters in your password?")
+  var ucInquiry = window.confirm("Do you want uppercase letters in your password?")
+  var numInquiry = window.confirm("Do you want numbers in your password?")
+  var scInquiry = window.confirm("Do you want special characters in your password?")
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+
+
+
+
+
 
   passwordText.value = password;
 
